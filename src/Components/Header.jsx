127811c212e3c8ecb8732ec9cb380338a2226aa5
Precious Michael <empreciousbaribe@gmail.com>
2025-10-31 22:52:27 +0100

    Updated portfolio section or added new features

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
 
     <div className='relative z-10 flex justify-center items-center container flex-col px-8 md:px-10'>
       <h1 className='font-semibold text-4xl md:6xl lg:7xl text-gray-400 '>Hi, I am <span className='text-yellow-400'>Michael Precious</span></h1>
-      <p class="mt-4 text-gray-300 ">I'm a frontend web developer. i 
-        will translate your designs into reality
+      <p class="mt-4 text-gray-300 ">I'm a frontend web developer, passionate about crafting beautifuland functional websites. I am proficient in HTML, CSS, TAILWIND and JAVASCRIPT 
+      
       </p>
     </div>
   
