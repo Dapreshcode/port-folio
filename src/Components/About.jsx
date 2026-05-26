874f9e769d8f0fc0fbb60aa8dcc755a32610c92d
Precious Michael <empreciousbaribe@gmail.com>
2026-05-26 07:42:42 +0100

    updated cv

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -33,7 +33,9 @@ const About = () => {
       </ul>
 
       <button className="mt-6 bg-yellow-400 text-black px-6 py-2 rounded-lg font-semibold hover:bg-yellow-500 transition">
-        <a href="Precious_Baribe_Web_Developer_CV (1).pdf">Download CV</a>
+        <a href="Precious_Baribe_Web_Developer_CV2.pdf" >
+          Download CV
+        </a>
       </button>
     </div>
   </motion.div>
